f061580ae4b8e6de71f0db2bf5dc517a0f4eb7ca
anvar2404 <anvarabdullaev14@gmail.com>
2020-06-01 16:01:01 +0600

    mb

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,8 @@
 
 let mobileBtn = document.querySelector('.burger-menu')
+let nav = document.querySelector('.top-show')
 
 mobileBtn.addEventListener('click', ()=>{
     mobileBtn.classList.toggle('show-menu')
-
+    nav.classList.toggle('show')
 })
